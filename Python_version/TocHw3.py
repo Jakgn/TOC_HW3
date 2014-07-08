bd006de1d5e99c4b7690fe72105ef87cf8789b7e
Jakgn <jakgn1234@hotmail.com>
2014-07-08 20:31:50 +0800

    add Python version

--- Python_version/TocHw3.py
+++ Python_version/TocHw3.py
@@ -1,6 +1,6 @@
 #coding=UTF-8
 # run instruction : python TocHw3.py URL AREA ROADNAME YEAR
-# ex			  : python TocHw3.py http://www.datagarage.io/api/5365dee31bc6e9d9463a0057 大安區 辛亥路 103
+# ex : python TocHw3.py http://www.datagarage.io/api/5365dee31bc6e9d9463a0057 大安區 辛亥路 103
 import sys
 import json
 import urllib2
@@ -9,7 +9,7 @@ page = urllib2.urlopen(sys.argv[1]).read()	## get web string
 data = json.loads(page)
 num = 0
 total = 0
-Area = unicode(sys.argv[2], 'utf-8')		##change to unicode because json.loads  , string is unicode
+Area = unicode(sys.argv[2], 'utf-8')		## change to unicode because json.loads  , string is unicode
 RoadName = unicode(sys.argv[3], 'utf-8')
 Year = sys.argv[4]
 for item in data:
